041de0bf8e2d041c455977e2e774a27a6767325a
giwawahab <giwawahabtomiwa2022@gmail.com>
2023-09-22 14:48:48 +0100

    Level 4 Security - using bcrypt hashing and salting for password encrypption

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -36,22 +37,25 @@ app.get("/register", function(req, res){
 });
 
 
-app.post("/register", async function(req, res){
-    
-    const username = req.body.username;
-    const password = md5(req.body.password);
-    
-    const newUser = new User({
-        email: username,
-        password: password
-    });
+app.post("/register", function(req, res){
 
-    const result = await newUser.save();
-    if(result){
-        res.render("secrets");
-    }else{
-        console.log("An error occurs");
-    }
+    bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+        // Store hash in your password DB.
+        const username = req.body.username;
+        const password = hash;
+        
+        const newUser = new User({
+            email: username,
+            password: password
+        });
+
+        const result = await newUser.save();
+        if(result){
+            res.render("secrets");
+        }else{
+            console.log("An error occurs");
+        }
+        });
 
 });
 
@@ -59,17 +63,22 @@ app.post("/register", async function(req, res){
 app.post("/login", async function(req, res){
     
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     const foundUser = await User.findOne({email: username});
     
     if(!foundUser){
         console.log("Cannot find the user");
     }else{
-        if(foundUser.password === password){
-            res.render("secrets");
-        }else{
-            console.log("Incorrect Password");
-        }
+
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            // result == true
+            if(result){
+                res.render("secrets");
+            }else{
+                console.log("Incorrect Password");
+            }
+        });
+        
     }
 
 });
